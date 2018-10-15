3a9224a062d3a05634f9815c97d359a07dc11769
William Wall <william@williamwall.ie>
2018-10-15 12:53:46 +0100

    Get donations endpoint tested

--- test/routes/donations-test.js
+++ test/routes/donations-test.js
@@ -5,7 +5,29 @@ let expect = chai.expect;
 
 chai.use(chaiHttp);
 let _ = require('lodash' );
+chai.use(require('chai-things'));
 
 describe('Donations', function (){
     // TODO
+});
+
+describe('Donations', function (){
+    describe('GET /donations',  () => {
+        it('should return all the donations in an array', function(done) {
+            chai.request(server)
+                .get('/donations')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(2);
+                    let result = _.map(res.body, (donation) => {
+                        return { id: donation.id,
+                            amount: donation.amount }
+                    });
+                    expect(result).to.include( { id: 1000000, amount: 1600  } );
+                    expect(result).to.include( { id: 1000001, amount: 1100  } );
+                    done();
+                });
+        });
+    });
 });
